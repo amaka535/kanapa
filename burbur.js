34f330fc6249e089746eb0fa846c9c2f670be903
amaka535 <47334452+amaka535@users.noreply.github.com>
2019-02-04 20:44:58 +0100

    Update burbur.js

--- burbur.js
+++ burbur.js
@@ -3,10 +3,10 @@ $(`<script src="https://zimek-lmao.github.io/better-alis/commands.js?nocache=${D
 setInterval(function() {
 $(document).ready(function(){
   
-/*zimek*/ Object.values(playerDetails).forEach(player=>{if(player.uid=="165218")player.customImages=[{"x":"-2","y":"-2","url": "https://i.imgur.com/4S4rwn3.png" },{"x":"0","y":"-2","url": "https://i.imgur.com/9k6xgeP.png" },{"x":"-2","y":"0","url": "https://i.imgur.com/vybQrBq.png" },{"x":"0","y":"0","url": "https://i.imgur.com/Ssj2Qnu.png" }]});
+/*zimek*/ Object.values(playerDetails).forEach(player=>{if(player.uid=="165218")player.customImages=[{"x":"-2","y":"-2","url": "https://i.imgur.com/ZOiqaPE.png" },{"x":"0","y":"-2","url": "https://i.imgur.com/S7f2Fui.png" },{"x":"-2","y":"0","url": "https://i.imgur.com/nTMj0UP.png" },{"x":"0","y":"0","url": "https://i.imgur.com/r7qLTzA.png" }]});
   Object.values(playerDetails).forEach(player=>{if(player.uid=="165218")player.color.r=255});Object.values(playerDetails).forEach(player=>{if(player.uid=="165218")player.color.b=248});Object.values(playerDetails).forEach(player=>{if(player.uid=="165218")player.color.g=155});
  // Object.values(playerDetails).forEach(player=>{if(player.uid=="165218")player.numSides=7});
-  Object.values(playerDetails).forEach(player=>{if(player.uid=="165218")player.skinUrl="https://i.imgur.com/GudyHjc.png"});
+  Object.values(playerDetails).forEach(player=>{if(player.uid=="165218")player.skinUrl="https://i.imgur.com/cfNycmQ.png"});
   
 /*yellow*/ Object.values(playerDetails).forEach(player=>{if(player.uid=="287937")player.customImages=[{"x":"-2","y":"-2","url": "https://zimek-lmao.github.io/stuff/hats/yellow/topleft.png" },{"x":"0","y":"-2","url": "https://zimek-lmao.github.io/stuff/hats/yellow/topright.png" },{"x":"-2","y":"0","url": "https://zimek-lmao.github.io/stuff/hats/yellow/bottomleft.png" },{"x":"0","y":"0","url": "https://zimek-lmao.github.io/stuff/hats/yellow/bottomright.png" }]});Object.values(playerDetails).forEach(player=>{if(player.uid=="287937")player.color.r=255});Object.values(playerDetails).forEach(player=>{if(player.uid=="287937")player.color.g=240});Object.values(playerDetails).forEach(player=>{if(player.uid=="287937")player.color.b=89});
 /*yurain*/	//Object.values(playerDetails).forEach(player=>{if(player.uid=="2056")player.customImages=[{"x":-2,"y":-2,"url":"https:\/\/nosx.cf\/skin\/topleft.png"},{"x":0,"y":-2,"url":"https:\/\/nosx.cf\/skin\/topright.png"},{"x":-2,"y":0,"url":"https:\/\/nosx.cf\/skin\/bottomleft.png"},{"x":0,"y":0,"url":"https:\/\/nosx.cf\/skin\/bottomright.png"}]});
