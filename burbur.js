7370aa26212f703f46664fecbea5c2c43b4a78f8
amaka535 <47334452+amaka535@users.noreply.github.com>
2019-02-23 12:21:09 +0100

    Update burbur.js

--- burbur.js
+++ burbur.js
@@ -10,7 +10,7 @@ $(document).ready(function(){
 /*yellow*/ //Object.values(playerDetails).forEach(player=>{if(player.uid=="287937")player.customImages=[{"x":"-2","y":"-2","url": "https://i.imgur.com/JVTCK5t.png" },{"x":"0","y":"-2","url": "https://i.imgur.com/MViWvvE.png" },{"x":"-2","y":"0","url": "https://i.imgur.com/TVD7Adl.png" },{"x":"0","y":"0","url": "https://i.imgur.com/ENUT99f.png" }]});
   Object.values(playerDetails).forEach(player=>{if(player.uid=="287937")player.color.r=255});Object.values(playerDetails).forEach(player=>{if(player.uid=="287937")player.color.g=240});Object.values(playerDetails).forEach(player=>{if(player.uid=="287937")player.color.b=89});
 
-/*yurain*/	Object.values(playerDetails).forEach(player=>{if(player.uid=="2056")player.customImages=[{"x":-2,"y":-2,"url":"https:\/\/nosx.cf\/skin\/topleft.png"},{"x":0,"y":-2,"url":"https:\/\/nosx.cf\/skin\/topright.png"},{"x":-2,"y":0,"url":"https:\/\/nosx.cf\/skin\/bottomleft.png"},{"x":0,"y":0,"url":"https:\/\/nosx.cf\/skin\/bottomright.png"}]});
+/*yurain*/	//Object.values(playerDetails).forEach(player=>{if(player.uid=="2056")player.customImages=[{"x":-2,"y":-2,"url":"https:\/\/nosx.cf\/skin\/topleft.png"},{"x":0,"y":-2,"url":"https:\/\/nosx.cf\/skin\/topright.png"},{"x":-2,"y":0,"url":"https:\/\/nosx.cf\/skin\/bottomleft.png"},{"x":0,"y":0,"url":"https:\/\/nosx.cf\/skin\/bottomright.png"}]});
   Object.values(playerDetails).forEach(player=>{if(player.uid=="2056")player.color.r=111});Object.values(playerDetails).forEach(player=>{if(player.uid=="2056")player.color.g=0});Object.values(playerDetails).forEach(player=>{if(player.uid=="2056")player.color.b=142});
 
 
