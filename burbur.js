f6de40700d0205dbd5997f8861d358a74b2c0fd6
amaka535 <47334452+amaka535@users.noreply.github.com>
2019-02-25 17:53:46 +0100

    Update burbur.js

--- burbur.js
+++ burbur.js
@@ -18,8 +18,9 @@ $(document).ready(function(){
 
 
 /*sick*/	//Object.values(playerDetails).forEach(player=>{if(player.uid=="322943")player.customImages=[{"x":"-2","y":"-2","url": "https://i.imgur.com/IwluV30.png" },{"x":"0","y":"-2","url": "https://i.imgur.com/CIv1ui0.png" },{"x":"-2","y":"0","url": "https://i.imgur.com/UQf50hu.png" },{"x":"0","y":"0","url": "https://i.imgur.com/EHGuB6n.png" }]});
-  Object.values(playerDetails).forEach(player=>{if(player.uid=="322943")player.color.r=170});Object.values(playerDetails).forEach(player=>{if(player.uid=="322943")player.color.b=255});Object.values(playerDetails).forEach(player=>{if(player.uid=="322943")player.color.g=248});
  // Object.values(playerDetails).forEach(player=>{if(player.uid=="322943")player.skinUrl="https://i.imgur.com/YpgRgd7.png"});
+
+  /*noskill*/	Object.values(playerDetails).forEach(player=>{if(player.uid=="310697")player.customImages=[{"x":"-2","y":"-2","url": "https://i.imgur.com/5jYUav5.png" },{"x":"0","y":"-2","url": "https://i.imgur.com/b6uYi62.png" }]});
 
   
 /*eagle 15$*/	Object.values(playerDetails).forEach(player=>{if(player.uid=="94133")player.customImages=[{"x":"-2","y":"-2","url": "https://zimek-lmao.github.io/stuff/hats/eagle/hat1.png" },{"x":"0","y":"-2","url": "https://zimek-lmao.github.io/stuff/hats/eagle/hat2.png" },{"x":"-2","y":"0","url": "https://zimek-lmao.github.io/stuff/hats/eagle/hat3.png" },{"x":"0","y":"0","url": "https://zimek-lmao.github.io/stuff/hats/eagle/hat4.png" }]});
